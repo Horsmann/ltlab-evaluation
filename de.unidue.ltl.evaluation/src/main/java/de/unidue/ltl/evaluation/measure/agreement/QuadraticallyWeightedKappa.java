eb74b576df25efaca4c2951ed69e9810dc6a9771
andreahorbach <andrea.horbach@uni-due.de>
2016-12-21 22:47:00 +0100

    removed duplicate kappa class

--- de.unidue.ltl.evaluation/src/main/java/de/unidue/ltl/evaluation/measure/agreement/QuadraticallyWeightedKappa.java
+++ de.unidue.ltl.evaluation/src/main/java/de/unidue/ltl/evaluation/measure/agreement/QuadraticallyWeightedKappa.java
@@ -16,28 +16,31 @@
  * limitations under the License.
  ******************************************************************************/
 
-package de.unidue.ltl.evaluation.measure;
+package de.unidue.ltl.evaluation.measure.agreement;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.dkpro.statistics.agreement.coding.CodingAnnotationStudy;
+import org.dkpro.statistics.agreement.coding.CohenKappaAgreement;
 import org.dkpro.statistics.agreement.coding.WeightedKappaAgreement;
 import org.dkpro.statistics.agreement.distance.IntervalDistanceFunction;
 
 import de.unidue.ltl.evaluation.EvaluationEntry;
 import de.unidue.ltl.evaluation.EvaluationResult;
+import de.unidue.ltl.evaluation.measure.EvaluationMeasure;
+import de.unidue.ltl.evaluation.measure.util.VectorPair;
 
 public class QuadraticallyWeightedKappa 
-	extends EvaluationMeasure<Double>
-{
+extends EvaluationMeasure<Double> {
 
 	public static final String KAPPA_MEASURE_QUADRATIC = "QuadraticallyWeightedKappa";
 
 	public QuadraticallyWeightedKappa(Collection<EvaluationEntry<Double>> entries) {
 		super(entries);
 	}
+
 
 	@Override
 	public Map<String, EvaluationResult> calculate() {
@@ -59,4 +62,6 @@ public class QuadraticallyWeightedKappa
 	public String getName() {
 		return KAPPA_MEASURE_QUADRATIC;
 	}
+
+
 }
---
+++ de.unidue.ltl.evaluation/src/test/java/de/unidue/ltl/evaluation/measure/KappaTest.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright 2016
+ * Language Technology Lab
+ * University of Duisburg-Essen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package de.unidue.ltl.evaluation.measure;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import org.junit.Test;
+
+import de.unidue.ltl.evaluation.EvaluationEntry;
+import de.unidue.ltl.evaluation.EvaluationResult;
+import de.unidue.ltl.evaluation.measure.agreement.LinearlyWeightedKappa;
+import de.unidue.ltl.evaluation.measure.agreement.QuadraticallyWeightedKappa;
+import de.unidue.ltl.evaluation.measure.util.CategorialMeasuresUtil;
+import de.unidue.ltl.evaluation.util.TestUtils;
+
+public class KappaTest {
+
+	// gold: 50 A, 18 B, 32 C
+	@Test
+	public void kappaTest(){
+		Collection<EvaluationEntry<Double>> entries = TestUtils.getExampleNumeric();
+
+		assertEquals(100, entries.size());
+		//	System.out.println(entries.size());
+
+		LinearlyWeightedKappa linWeightedKappa = new LinearlyWeightedKappa(entries);
+		Map<String, EvaluationResult> results = linWeightedKappa.calculate();
+		
+		EvaluationResult result = results.get(LinearlyWeightedKappa.class.getSimpleName());
+		System.out.println("Linearlyly Weighted Kappa: "+result.getResult());
+		assertEquals(0.3975, result.getResult(), 0.001);
+		
+		QuadraticallyWeightedKappa weightedKappa = new QuadraticallyWeightedKappa(entries);
+		results = weightedKappa.calculate();
+		
+		result = results.get(QuadraticallyWeightedKappa.class.getSimpleName());
+		System.out.println("Quadraticaly Weighted Kappa: "+result.getResult());
+		assertEquals(0.3492, result.getResult(), 0.001);
+
+		
+
+	}
+}
