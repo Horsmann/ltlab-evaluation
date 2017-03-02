5c9cfed19411f94828c9712f166beddc4042c551
Torsten Zesch <torsten.zesch@gmail.com>
2017-03-02 14:13:51 +0100

    Closes issue #3

--- de.unidue.ltl.evaluation/src/main/java/de/unidue/ltl/evaluation/transformation/EvaluationTransformer.java
+++ de.unidue.ltl.evaluation/src/main/java/de/unidue/ltl/evaluation/transformation/EvaluationTransformer.java
@@ -19,13 +19,24 @@ package de.unidue.ltl.evaluation.transformation;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import de.unidue.ltl.evaluation.Evaluation;
 import de.unidue.ltl.evaluation.EvaluationEntry;
 
+/**
+ * @author zesch
+ *
+ */
 public class EvaluationTransformer {
 
+	/**
+	 * Delete selected labels from evaluation
+	 * @param eval An evaluation
+	 * @param labels A list of labels to be deleted.
+	 * @return A new evaluation object without the selected labels
+	 */
 	public static <T> Evaluation<T> deleteLabels(Evaluation<T> eval, T ... labels) {
 		
 		Evaluation<T> transformed = new Evaluation<T>();		
@@ -41,5 +52,47 @@ public class EvaluationTransformer {
 		
 		return transformed;
 	}
-
+	
+	
+	/**
+	 * @param eval An evaluation
+	 * @param mapping A mapping from gold values to be changed to new gold values
+	 * @return An evaluation with changed gold labels
+	 */
+	public static <T> Evaluation<T> changeGoldLabel(Evaluation<T> eval, Map<T,T> mapping) {
+		
+		Evaluation<T> transformed = new Evaluation<T>();		
+						
+		for (EvaluationEntry<T> entry : eval.getEntries()) {			
+			if (mapping.containsKey(entry.getGold())) {
+				transformed.register(mapping.get(entry.getGold()), entry.getPredicted());				
+			}
+			else {
+				transformed.register(entry.getGold(), entry.getPredicted());
+			}
+		}
+		
+		return transformed;
+	}
+	
+	/**
+	 * @param eval An evaluation
+	 * @param mapping A mapping from predicted values to be changed to new predicted values
+	 * @return An evaluation with changed predicted labels
+	 */
+	public static <T> Evaluation<T> changePredictedLabel(Evaluation<T> eval, Map<T,T> mapping) {
+		
+		Evaluation<T> transformed = new Evaluation<T>();		
+						
+		for (EvaluationEntry<T> entry : eval.getEntries()) {			
+			if (mapping.containsKey(entry.getPredicted())) {
+				transformed.register(entry.getGold(), mapping.get(entry.getPredicted()));				
+			}
+			else {
+				transformed.register(entry.getGold(), entry.getPredicted());
+			}
+		}
+		
+		return transformed;
+	}
 }
