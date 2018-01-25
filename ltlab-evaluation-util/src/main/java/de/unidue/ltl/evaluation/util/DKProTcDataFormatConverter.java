ab47700b7aea1b8d2a5beeeeb7a02d3d489e46b2
Tobias Horsmann <tobias.horsmann@gmail.com>
2018-01-25 16:05:40 +0100

    re-added classes

---
+++ ltlab-evaluation-util/src/main/java/de/unidue/ltl/evaluation/util/DKProTcDataFormatConverter.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright 2018
+ * Language Technology Lab
+ * University of Duisburg-Essen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.unidue.ltl.evaluation.util;
+
+import java.io.File;
+
+import de.unidue.ltl.evaluation.core.EvaluationData;
+
+public class DKProTcDataFormatConverter<T> {
+
+	/**
+	 * Loads a DKPro TC id2outcome file into the evaluation data format
+	 * @param id2OutcomeFile
+	 * @return
+	 * 		an evaluation data object
+	 */
+	public EvaluationData<T> convertId2Outcome(File id2OutcomeFile){
+		
+		return null;
+	}
+	
+}
