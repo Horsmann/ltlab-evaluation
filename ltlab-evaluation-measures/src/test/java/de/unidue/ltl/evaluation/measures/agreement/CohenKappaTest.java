d564004af4df7ec33fe6ea73c63477ad08327a47
Tobias Horsmann <tobias.horsmann@gmail.com>
2018-01-30 09:10:51 +0100

    added more test cases

---
+++ ltlab-evaluation-measures/src/test/java/de/unidue/ltl/evaluation/measures/agreement/CohenKappaTest.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright 2018
+ * Language Technology Lab
+ * University of Duisburg-Essen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.unidue.ltl.evaluation.measures.agreement;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import de.unidue.ltl.evaluation.core.EvaluationData;
+
+public class CohenKappaTest {
+	
+	@Test
+	public void agreementTest(){
+
+		EvaluationData<String> data = new EvaluationData<>();
+		data.register("A", "B");
+		data.register("B", "B");
+		data.register("A", "A");
+		data.register("A", "A");
+		
+		assertEquals(0.5, new CohenKappa<String>(data).getResult(), 0.001);
+				
+	}
+}
---
+++ ltlab-evaluation-measures/src/test/java/de/unidue/ltl/evaluation/measures/agreement/ScottPiTest.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright 2018
+ * Language Technology Lab
+ * University of Duisburg-Essen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package de.unidue.ltl.evaluation.measures.agreement;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import de.unidue.ltl.evaluation.core.EvaluationData;
+import de.unidue.ltl.evaluation.testing.TestUtils;
+
+public class ScottPiTest {
+	
+	@Test
+    public void kappaTest(){
+        EvaluationData<Double> entries = new EvaluationData<>(TestUtils.getExampleNumeric());
+        assertEquals(0.4523, new ScottPi<Double>(entries).getResult(), 0.001);
+    }
+}
